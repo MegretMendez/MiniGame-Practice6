d4478fcd3d27e0c7c4c37f8c7c5a47b715ee2c52
MegretMendez <142510070+MegretMendez@users.noreply.github.com>
2023-11-08 23:08:55 -0400

    Update WaveManager.cs

--- WaveManager.cs
+++ WaveManager.cs
@@ -5,30 +5,37 @@ using UnityEngine.Events;
 
 public class WaveManager : MonoBehaviour
 {
-    public static WaveManager instance;
-    public List<WaveSpawn> waves;
-    public UnityEvent onChanged;
-   
+    public static WaveManager instance;   // A static reference to the WaveManager instance.
+    public List<WaveSpawn> waves;         // A list to store information about different waves.
+    public UnityEvent onChanged;           // A Unity Event to trigger when waves are added or removed.
+
     void Awake()
     {
-        if(instance == null){
-            instance = this;
+        // This function is called when the script is loaded or initialized.
+
+        if (instance == null)
+        {
+            instance = this;  // If there's no existing instance, set this as the reference.
         }
-        else{
-            Debug.LogError("Duplicated Waves Manager");
+        else
+        {
+            Debug.LogError("Duplicated Waves Manager");  // Log an error if there's more than one instance.
         }
-        
     }
 
-    public void addWave(WaveSpawn wave){
-        waves.Add(wave);
-        onChanged.Invoke();
-        
+    public void addWave(WaveSpawn wave)
+    {
+        // This function is used to add a new wave to the list.
+
+        waves.Add(wave);  // Add the new wave to the list.
+        onChanged.Invoke();  // Trigger the onChanged event to notify other scripts that a change has occurred.
     }
 
-    public void removeWave(WaveSpawn wave){
-        waves.Remove(wave);
-        onChanged.Invoke();
-    }
+    public void removeWave(WaveSpawn wave)
+    {
+        // This function is used to remove a wave from the list.
 
+        waves.Remove(wave);  // Remove the specified wave from the list.
+        onChanged.Invoke();  // Trigger the onChanged event to notify other scripts of the change.
+    }
 }
